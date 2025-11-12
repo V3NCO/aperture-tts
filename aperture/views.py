b48c53dc89dce5cf086955c62c75e73ee85bdc5b
Venco <v3nco@v3nco.dev>
2025-11-12 20:50:19 +0100

    Document all the available styles

--- aperture/views.py
+++ aperture/views.py
@@ -1,17 +1,18 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Query
 from fastapi.responses import Response
 from pydantic import BaseModel
 from aperture.services import ModelService
 from aperture.app_services import get_model
 from IPython.display import Audio
+from typing import Literal
 
 api_router = APIRouter()
 
 class ModelParams(BaseModel):
     text: str
-    style: str = "Neutral"
+    style: Literal["Neutral", "Deep", "Standard", "Standard_02", "Light"] = Query(default="Neutral")
 
-@api_router.get('/', response_class=Response)
+@api_router.get('/tts', response_class=Response)
 async def index(
     params: ModelParams,
     model: ModelService = Depends(get_model)
