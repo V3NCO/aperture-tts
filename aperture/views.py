8ecb520982e24a0a5c112d1ca5692960a8968c64
Venco <v3nco@v3nco.dev>
2025-11-12 19:36:33 +0100

    took me a while to figure out i can just say media type wav and have it just work like that

--- aperture/views.py
+++ aperture/views.py
@@ -1,10 +1,22 @@
 from fastapi import APIRouter, Depends
+from fastapi.responses import Response
+from pydantic import BaseModel
 from aperture.services import ModelService
 from aperture.app_services import get_model
+from IPython.display import Audio
 
 api_router = APIRouter()
 
-@api_router.get('/')
-async def index(model: ModelService = Depends(get_model)):
-    print(model.generate_audio("test"))
-    return "success"
+class Textinput(BaseModel):
+    text: str
+    aa: str
+
+@api_router.get('/', response_class=Response)
+async def index(
+    text: Textinput,
+    model: ModelService = Depends(get_model)
+):
+    sr,audio = model.generate_audio(text.text)
+    print(sr)
+    print(audio)
+    return Response(content=Audio(audio, rate=sr).data, media_type="audio/wav")
