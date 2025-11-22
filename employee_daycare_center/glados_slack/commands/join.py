d0494e3c741bd266eed7a4560390a58579b9df96
Venco <v3nco@v3nco.dev>
2025-11-22 16:24:18 +0100

    Check if already in huddle

--- employee_daycare_center/glados_slack/commands/join.py
+++ employee_daycare_center/glados_slack/commands/join.py
@@ -14,15 +14,18 @@ async def join_handler(
     await ack()
     from glados_slack.env import env
 
-    payload = {
-            "token": config.slack.userbot_token,
-            "channel_id": channel,
-            "region": "us-east-2" # Yes we are hardcoding us-east-2 because it's always us-east; just like it's always DNS :p
-    }
-    headers = {"Cookie": f"d={config.slack.userbot_d};"}
-    huddle = await env.http.post("https://blahaj.enterprise.slack.com/api/rooms.join", data=payload, headers=headers)
-    response = await huddle.json()
-    print(response)
-    await respond(f"Joining huddle in <#{response['huddle']['channels'][0]}|>")
-    await env.http.post("http://localhost:7171/join", json={"meeting": response["call"]["free_willy"]["meeting"], "attendee": response["call"]["free_willy"]["attendee"]})
-    await CurrentHuddles.insert(CurrentHuddles(channel_id=response['huddle']['channels'][0], thread_ts=response['huddle']['thread_root_ts'])).run()
+    if not CurrentHuddles.exists().where(CurrentHuddles.channel_id == channel):
+        payload = {
+                "token": config.slack.userbot_token,
+                "channel_id": channel,
+                "region": "us-east-2" # Yes we are hardcoding us-east-2 because it's always us-east; just like it's always DNS :p
+        }
+        headers = {"Cookie": f"d={config.slack.userbot_d};"}
+        huddle = await env.http.post("https://blahaj.enterprise.slack.com/api/rooms.join", data=payload, headers=headers)
+        response = await huddle.json()
+        print(response)
+        await env.http.post("http://localhost:7171/join", json={"meeting": response["call"]["free_willy"]["meeting"], "attendee": response["call"]["free_willy"]["attendee"]})
+        await CurrentHuddles.insert(CurrentHuddles(channel_id=response['huddle']['channels'][0], thread_ts=response['huddle']['thread_root_ts'])).run()
+        await respond(f"Joined huddle in <#{response['huddle']['channels'][0]}|> :3c:")
+    else:
+        await respond("I am already in this huddle :neodog_pat:")
